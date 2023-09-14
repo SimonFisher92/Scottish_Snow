30dedf0b9b619de8656292653f448a7b5a94565b
Murray Cutforth <mcc74@cam.ac.uk>
2023-09-14 15:14:01 -0700

    #5: Modified download script to be able to pass the filter condition from the command line, so it can be used to download any bands/resolution desired. Added example for 10m RGB bands.

--- src/download/download.py
+++ src/download/download.py
@@ -26,7 +26,7 @@ def _check_previous_download(cdir, title):
     return title in downloaded_titles
 
 
-def _download_prod_inner(prod_id, product_info, cdir, api, download_full, retry_delay=1800):
+def _download_prod_inner(prod_id, product_info, cdir, api, path_filter, retry_delay=1800):
     """This function tries to download a given product. Will retry every 30 mins until download succeeds. In particular, this function will keep retrying if a product is offline.
     """
     if _check_previous_download(cdir, product_info["title"]):
@@ -37,12 +37,8 @@ def _download_prod_inner(prod_id, product_info, cdir, api, download_full, retry_
 
     while True:
         try:
-            if download_full:
-                api.download(prod_id, cdir)
-            else:
-                # TODO: extend filtering, so we have option to download other bands?
-                path_filter = make_path_filter("*SCL_20m.jp2")
-                api.download(prod_id, cdir, nodefilter=path_filter)
+            path_filter = make_path_filter(path_filter)
+            api.download(prod_id, cdir, nodefilter=path_filter)
             break
         except Exception as e:
             logger.error(f"Download failed with {e}. Retrying in 30mins.")
@@ -63,7 +59,7 @@ def download_products(prod_ids: List, args: argparse.Namespace):
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
         for pid in prod_ids:
-            executor.submit(_download_prod_inner, pid, product_infos[pid], args.data_dir, api, args.download_full)
+            executor.submit(_download_prod_inner, pid, product_infos[pid], args.data_dir, api, args.product_filter)
 
     logger.info("All products downloaded")
 
--- src/download/main.py
+++ src/download/main.py
@@ -17,15 +17,16 @@ logger = logging.getLogger(__name__)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="A script to download Sentinel-2 data for snowpatch analysis")
+    parser = argparse.ArgumentParser(
+            description="A script to download Sentinel-2 data for snowpatch analysis. Example command: \npython -m src.download.main --data_dir='data' --geojson_path='input/cairngorms_footprint.geojson' --product_filter='*B0[234]_10m.jp2' --target_tile='T30VVJ' --api_user=<> --api_password=<>")
 
-    parser.add_argument("--data_dir", type=str, help="Path to dir where data should be cached", default=".")
-    parser.add_argument("--geojson_path", type=str, help="Path to geojson file containing polygons covering all areas which data should be downloaded for")
-    parser.add_argument("--download_full", action="store_true", help="Store the full data product, rather than just the 20m SCL band. Uses WAY more disk space.")
+    parser.add_argument("--data_dir", type=str, required=True, help="Path to dir where data should be cached", default=".")
+    parser.add_argument("--geojson_path", type=str, required=True, help="Path to geojson file containing polygons covering all areas which data should be downloaded for")
+    parser.add_argument("--product_filter", type=str, default="*SCL_20m.jp2", help=r"Path filter which is passed to sentinelsat.SentinelAPI.download(). The default is '*SCL_20m.jp2' which only downloads SCL masks at 20m resolution. For all data use '*'. For 10m resolution RGB use '*B0[234]_10m.jp2'. See documentation at https://sentinelsat.readthedocs.io/en/latest/api_overview.html#downloading-parts-of-products.")
     parser.add_argument("--target_tile", type=str, default=None, help="Optional field to restrict data to a single tile, such as T30VVJ")
-    parser.add_argument("--max_cloud_cover", type=int, default=50, help="Only get results with total cloud cover % less than this")
-    parser.add_argument("--api_user", type=str, help="Username for Copernicus Sentinel API")
-    parser.add_argument("--api_password", type=str, help="Password for Copernicus Sentinel API")
+    parser.add_argument("--max_cloud_cover", type=int, default=50, help="Only get results with total cloud cover %% less than this")
+    parser.add_argument("--api_user", type=str, required=True, help="Username for Copernicus Sentinel API")
+    parser.add_argument("--api_password", type=str, required=True, help="Password for Copernicus Sentinel API")
 
     args = parser.parse_args()
     return args
