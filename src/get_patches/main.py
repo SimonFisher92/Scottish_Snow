e2eba5a6f8fcd19850d125f34b91ed079dba45e0
Murray Cutforth <mcc74@cam.ac.uk>
2023-10-12 12:59:03 -0700

    Minor changes to get_patches, to get the data on patch centre and polygons needed for streamlit app

--- src/get_patches/main.py
+++ src/get_patches/main.py
@@ -5,7 +5,7 @@ import math
 logging.basicConfig(level=logging.INFO)
 
 
-def snowpatch_geojson(coords_dict: dict, offset: float):
+def snowpatch_bbox_geojson(coords_dict: dict, offset: float):
     '''
     :param coords_dict: input patch names and coordinates
     :param offset: the offset from the centre of the snowpatch required to the make the bounding box ROI (larger is bigger)
@@ -46,7 +46,7 @@ def snowpatch_geojson(coords_dict: dict, offset: float):
         all_data.append(geojson_data)
 
         # Save individual GeoJSON to a file
-        with open(f'{name}.geojson', 'w') as f:
+        with open(f'{name}_bbox.geojson', 'w') as f:
             json.dump(geojson_data, f, indent=4)
 
 
@@ -57,7 +57,46 @@ def snowpatch_geojson(coords_dict: dict, offset: float):
         "features": all_data
     }
 
-    with open('all_patches.geojson', 'w') as f:
+    with open('all_patches_bbox.geojson', 'w') as f:
+        json.dump(all_geojson, f, indent=4)
+
+
+def snowpatch_point_geojson(coords_dict: dict):
+    '''
+    :param coords_dict: input patch names and coordinates
+    :param offset: the offset from the centre of the snowpatch required to the make the bounding box ROI (larger is bigger)
+    :return: geojsons for specific patches
+    '''
+
+    all_data = []
+
+    for name, centroid in coords_dict.items():
+
+        # Create GeoJSON
+        geojson_data = {
+            "type": "Feature",
+            "properties": {"name": f"{name}"},
+            "geometry": {
+                "type": "Point", #arbitrary
+                "coordinates": centroid
+            }
+        }
+
+        all_data.append(geojson_data)
+
+        # Save individual GeoJSON to a file
+        with open(f'{name}_point.geojson', 'w') as f:
+            json.dump(geojson_data, f, indent=4)
+
+
+    # Save all patches to one geojson file
+
+    all_geojson = {
+        "type": "FeatureCollection",
+        "features": all_data
+    }
+
+    with open('all_patches_point.geojson', 'w') as f:
         json.dump(all_geojson, f, indent=4)
 
 
@@ -87,5 +126,6 @@ def get_coords_from_file(filepath: str) -> dict:
 if __name__ == "__main__":
     test_cords = get_coords_from_file('bluebird_coords')
     logging.info(test_cords)
-    snowpatch_geojson(test_cords, offset=1e-3)
+    snowpatch_bbox_geojson(test_cords, offset=1e-2)
+    snowpatch_point_geojson(test_cords)
 
